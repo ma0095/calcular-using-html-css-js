2f3fa347cfb19a631d09c3a971df391bb031c1ba
arun <arunmadambathl@gmail.com>
2022-08-12 19:50:40 +0530

    cal1

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayNumber(num){
+    id_result.value +=num
+}
+
+function evaluateExpresion(){
+    // let curval=id_result.value
+    // let res=eval(curval)
+    // id_result.value=res
+    id_result.value=eval(id_result.value)
+}
+
+function backSpace(){
+    id_result.value=id_result.value.slice(0,-1)
+}
+
+function clearBox(){
+    id_result.value=""
+}
